c85e1017036cb31e79b719d7acfa6225a1db4379
TGNYC <TGNYC@users.noreply.github.com>
2016-06-16 09:35:55 -0400

    Update TextAnalysis.java

--- TextAnalysis.java
+++ TextAnalysis.java
@@ -1,2 +1,21 @@
 // Date: 6/13/16
 
+public static void main(String[] args) throws FileNotFoundException {
+        String fileName;    
+        String letter;      
+        int total = 0;     
+        
+        Scanner in = new Scanner(System.in);
+        fileName = in.next();
+        File file = new File(fileName);
+        Scanner inputFile = new Scanner(file);  
+
+        int count = 0;
+            
+        if (inputFile.nextLine().equalsIgnoreCase(letter)) {                                
+                    
+            count++;          
+                
+            total += count;   
+        }
+    }
