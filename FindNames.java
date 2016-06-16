90fae1eef43ffd0d44ccc1f81d21f411d60dc6a6
TGNYC <TGNYC@users.noreply.github.com>
2016-06-16 09:38:55 -0400

    Update FindNames.java

--- FindNames.java
+++ FindNames.java
@@ -1,1 +1,15 @@
 // 06/11/16
+
+package findnames;
+import java.util.Scanner;
+
+public class FindNames {
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String first = console.next();
+        String last = console.next();
+        System.out.println(last + ", " + first);
+    }
+    
+}
